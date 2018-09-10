53d4e693d42a18eb67826aa5e68d2e5b280b4507
João Thallis <joao.thallis@stone.com.br>
2018-09-10 01:25:31 -0300

    Create auto git core
    Notifier, add, commit, and push

---
+++ add-commit-push.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Add, commit and push
+git add . && git commit -m "backup" && git push
---
+++ auto-git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Start inotifywait and add-commit-push.sh
+while true; do 
+  inotifywait -r -e modify,attrib,close_write,move,create,delete . && /bin/bash add-commit-push.sh
+done
